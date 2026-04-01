94ef146d35bd672a7103216eddacb6903b49c4a7
Prashant Sinha <kpkr12398@gmail.com>
2026-04-01 05:43:16 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length!= t.Length) return false;
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        foreach(char ch in s){
+            if(dict.ContainsKey(ch)) dict[ch]++;
+            else dict[ch]=1;
+        }
+
+        foreach(char ch in t){
+            
+            if(dict.ContainsKey(ch)) {
+                
+                dict[ch]--;
+                if(dict[ch]<0) return false;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
